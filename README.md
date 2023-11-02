35e48448b2d2e6ea510624a9aa43062f6e4524f2
Karl Webster <19327709+ktasper@users.noreply.github.com>
2023-11-02 19:32:07 +0000

    Added UI

--- README.md
+++ README.md
@@ -4,13 +4,20 @@ FMDS is a Python script designed for Football Manager data analysis and reportin
 First thing first, Thanks to [squirrel_plays](https://www.youtube.com/@squirrel_plays_fof4318) for the Inspiration and their hard work on the football manager views, Their code is under `./original.py`.
 
 ## Prerequisites
-- Python 3.x installed on your system.
-- Required Python libraries: `pandas`.
+- Windows 11 (May work on 10 & 7)
 ## Usage
 1. Export Data from Football Manager
-- Export squad data from Football Manager and save it in the specified directory.
-2. Configure Views (Optional)
-- Define the columns you want to include in the final report by editing the views.json file. Ensure that the column names match the exported data headers.
+- Export squad data from Football Manager and save it to a folder.
+    - I use the `all_attribute_squad` for my squad view.
+    - Select all players with shift click
+    - `CTRL + P` > Web Page
+    - Save this file where-ever you want, I have a folder on my Desktop called `Documents/FMDS-Data` so its easy to do in the FM UI.
+        - Don't forget to give it a name you understand.
+- Edit the `config.json` with your path to your FMDS-Data folder.
+
+3. Define Position Weights (Optional)
+- Customize player attribute weights for different positions by editing the positions.json file. Follow the specified format to define scoring categories such as "essential," "core," and "secondary."
+    - You can add as many or as little categories as you want.
 
 Supported names are:
 ```
@@ -23,21 +30,29 @@ Supported names are:
     "Tec", "Thr", "TRO", "Vis", "Wor", "UID", "Cor", "Club"
 ]
 ```
-3. Define Position Weights (Optional)
-- Customize player attribute weights for different positions by editing the positions.json file. Follow the specified format to define scoring categories such as "essential," "core," and "secondary."
-    - You can add as many or as little categories as you want.
+
+We also create a few custom metrics:
+```
+"Spd" = "Pac" + "Acc" / 2
+"Work" ="Wor" + "Sta" / 2
+"SetP" ="Jum" + "Bra" / 2
+```
+
+
 4. Run the Script
 - Execute the fm_data_science.py script.
-```
-python fm_data_science.py
-```
+- Run the `fmds.exe`
+    - or `python main.py`
 The script will process the exported data, calculate player scores based on position weights, and generate an interactive HTML report.
 
 5. Generated HTML Report
-- The script generates an HTML report (output.html) with DataTables integration. Open this file in a web browser to view the interactive report.
+- The script generates an HTML report (output.html) with DataTables integration. Open this file in a web browser to view the interactive report. The file gets saved to the `generated_data` folder
 
 ## Customization
 
 ### Modifying Position Weights
 
 Customize player attribute weights for different positions by editing the positions.json file. Update the attributes and weights based on your game preferences.
+
+## Notes
+The script only works on the latest file in the `export_path`.
--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import glob
 import json
 import os
 import uuid
+import logging
 import pandas as pd
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+
+# Default configuration
+config_data = {"export_path": "", "output_directory": ""}
 
 
-def generate_html(dataframe: pd.DataFrame):
+def gen_html(dataframe: pd.DataFrame):
     # get the table HTML from the dataframe
     table_html = dataframe.to_html(table_id="table", index=False)
     # construct the complete HTML with jQuery Data tables
@@ -35,7 +42,7 @@ def generate_html(dataframe: pd.DataFrame):
     return html
 
 
-def weight_calc(position, category_id):
+def weight_calc(position, category_id, position_weights: json, squad_rawdata):
     """
     This takes in a position as defined in the json
     And a category_id (IE: essential, core, secondary) and provides a consistent way of calculating a players ability
@@ -56,14 +63,14 @@ def weight_calc(position, category_id):
             len(position_weights["positions"][position][category_id]["attributes"])
             * weight
         )
-        #print(
+        # print(
         #    f"""
         #      Debug:
         #      Position: {position}
         #      Cat: {category_id}
         #      Debug weight: {weight}
         #      normaliser: {normaliser}"""
-        #)
+        # )
         attribute_values = (
             sum(squad_rawdata[attribute] for attribute in attributes) * weight
         )
@@ -73,161 +80,215 @@ def weight_calc(position, category_id):
         else:
             return 0
     else:
-        #print(
+        # print(
         #    f"""
         #      Debug:
         #      Position: {position}
         #      Cat: {category_id}
         #      Debug weight: Not set (0)
         #      normaliser: Not Set (0)"""
-        #)
+        # )
         return 0
 
 
-# finds most recent file in specified folder
-try:
-    with open("config.json", "r") as config_file:
-        config_data = json.load(config_file)
-except FileNotFoundError:
-    print ("Could not find the config.json file")
-    os._exit(1)
-export_dir: str = config_data["export_path"]
+def main(export_path, output_path):
+    # finds most recent file in specified folder
+    export_dir: str = export_path
+
+    list_of_files = glob.glob(os.path.join(export_dir, "*"))
+    try:
+        latest_file = max(list_of_files, key=os.path.getctime)
+        latest_filename = os.path.basename(latest_file).replace(".html", "")
+    except ValueError:
+        print(f"No files found in {export_dir}")
+        latest_filename = None
+
+    # print(f"Debug Latest File: {latest_filename}")
+
+    # Read HTML file exported by FM - in this case an example of an output from the squad page
+    # This reads as a list, not a dataframe
+    squad_rawdata_list: list = pd.read_html(
+        latest_file, header=0, encoding="utf-8", keep_default_na=False
+    )
+
+    # transform the list into a dataframe
+    squad_rawdata = squad_rawdata_list[0]
+
+    # Calculate simple speed and workrate scores
+    squad_rawdata["Spd"] = (squad_rawdata["Pac"] + squad_rawdata["Acc"]) / 2
+    squad_rawdata["Work"] = (squad_rawdata["Wor"] + squad_rawdata["Sta"]) / 2
+    squad_rawdata["SetP"] = (squad_rawdata["Jum"] + squad_rawdata["Bra"]) / 2
+
+    # Keep a list of all the headers we want to use in the final view
+    final_view_headers = []
+
+    # Add all the exported view names for the final view
+    allowed_view_items = [
+        "Reg",
+        "Inf",
+        "Name",
+        "Age",
+        "Wage",
+        "Transfer Value",
+        "Nat",
+        "2nd Nat",
+        "Position",
+        "Personality",
+        "Media Handling",
+        "Av Rat",
+        "Left Foot",
+        "Right Foot",
+        "Height",
+        "1v1",
+        "Acc",
+        "Aer",
+        "Agg",
+        "Agi",
+        "Ant",
+        "Bal",
+        "Bra",
+        "Cmd",
+        "Cnt",
+        "Cmp",
+        "Cro",
+        "Dec",
+        "Det",
+        "Dri",
+        "Fin",
+        "Fir",
+        "Fla",
+        "Han",
+        "Hea",
+        "Jum",
+        "Kic",
+        "Ldr",
+        "Lon",
+        "Mar",
+        "OtB",
+        "Pac",
+        "Pas",
+        "Pos",
+        "Ref",
+        "Spd",
+        "Work",
+        "Sta",
+        "Str",
+        "Tck",
+        "Tea",
+        "Tec",
+        "Thr",
+        "TRO",
+        "Vis",
+        "Wor",
+        "UID",
+        "Cor",
+        "Club",
+    ]
+
+    # Add the views from the "views.json" to the final views list if its in the allowed list
+    try:
+        with open("views.json", "r") as views_json:
+            core_views = json.load(views_json)
+    except FileNotFoundError:
+        print("Could not find the views.json file")
+    for view_item in core_views["order"]:
+        if view_item in allowed_view_items:
+            final_view_headers.append(view_item)
+        else:
+            print(f"Ignoring {view_item} since it is not in the allowed views list")
+
+    # Load position weights from JSON file
+    try:
+        with open("positions.json", "r") as json_file:
+            position_weights = json.load(json_file)
+
+            # Retrieve position category from JSON configuration
+            position_category = list(
+                position_weights["positions"].keys()
+            )  # Get the key from the "positions" dictionary
+            for position in position_category:
+                squad_rawdata[f"{position}"] = 0
+                # Now get each scoring category from the json ie: "core, essential etc"
+                scoring_categories = list(
+                    position_weights["positions"][position].keys()
+                )
+                for category in scoring_categories:
+                    # Add the data back to the data frame after we have run some calculations on it
+                    squad_rawdata[f"{position}"] += weight_calc(
+                        position, category, position_weights, squad_rawdata
+                    )
+                squad_rawdata[position] = squad_rawdata[position].round(1)
+                final_view_headers.append(position)
+    except FileNotFoundError:
+        print("Could not find the positions.json file")
+
+    squad = squad_rawdata[final_view_headers]
+    # Create the dir if it does not exist
+    if not os.path.exists(f"{output_path}/generated_data/"):
+        os.makedirs(f"{output_path}/generated_data/")
+    # generates random file name for write-out of html file based on the file we use
+
+    filename = str(
+        f"{output_path}/generated_data/{latest_filename}-{uuid.uuid4()}.html"
+    )
+    # creates a sortable html export from the dataframe 'squad'
+    html = gen_html(squad)
+    open(filename, "w", encoding="utf-8").write(html)
 
 
-list_of_files = glob.glob(os.path.join(export_dir, "*"))
-try:
-    latest_file = max(list_of_files, key=os.path.getctime)
-    latest_filename = os.path.basename(latest_file).replace('.html', '')
-except ValueError:
-    print(f"No files found in {export_dir}")
-    latest_filename = None
+# Initialize Tkinter
+root = tk.Tk()
+root.title("Dashboard")
 
-#print(f"Debug Latest File: {latest_filename}")
+# Create a label for feedback
+feedback_label = tk.Label(root, text="")
+feedback_label.pack(pady=10)
 
-# Read HTML file exported by FM - in this case an example of an output from the squad page
-# This reads as a list, not a dataframe
-squad_rawdata_list: list = pd.read_html(
-    latest_file, header=0, encoding="utf-8", keep_default_na=False
+
+# Function to select export path using file dialog
+def select_export_path():
+    global export_path
+    export_path = filedialog.askdirectory()
+    export_path_label.config(text="Export Path: " + export_path)
+
+
+# Function to select output directory using file dialog
+def select_output_dir():
+    global output_dir
+    output_dir = filedialog.askdirectory()
+    output_dir_label.config(text="Output Directory: " + output_dir)
+
+# Function to generate the HTML and perform other operations
+def generate_html():
+    try:
+        main(export_path, output_dir)
+        logging.info("Action performed successfully")
+        feedback_label.config(text="Action completed successfully")
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
+        feedback_label.config(text=f"Error: {str(e)}")
+        messagebox.showerror("Error", f"An error occurred: {str(e)}")
+
+# UI Elements
+export_path_button = tk.Button(
+    root, text="Select Export Path", command=select_export_path
 )
+export_path_button.pack(pady=10)
 
-# transform the list into a dataframe
-squad_rawdata = squad_rawdata_list[0]
+export_path_label = tk.Label(root, text="Export Path: Not Selected")
+export_path_label.pack(pady=10)
 
+output_dir_button = tk.Button(
+    root, text="Select Output Directory", command=select_output_dir
+)
+output_dir_button.pack(pady=10)
 
-# Calculate simple speed and workrate scores
-squad_rawdata["Spd"] = (squad_rawdata["Pac"] + squad_rawdata["Acc"]) / 2
-squad_rawdata["Work"] = (squad_rawdata["Wor"] + squad_rawdata["Sta"]) / 2
-squad_rawdata["SetP"] = (squad_rawdata["Jum"] + squad_rawdata["Bra"]) / 2
-
-
-# Keep a list of all the headers we want to use in the final view
-final_view_headers = []
-
-# Add all the exported view names for the final view
-allowed_view_items = [
-    "Reg",
-    "Inf",
-    "Name",
-    "Age",
-    "Wage",
-    "Transfer Value",
-    "Nat",
-    "2nd Nat",
-    "Position",
-    "Personality",
-    "Media Handling",
-    "Av Rat",
-    "Left Foot",
-    "Right Foot",
-    "Height",
-    "1v1",
-    "Acc",
-    "Aer",
-    "Agg",
-    "Agi",
-    "Ant",
-    "Bal",
-    "Bra",
-    "Cmd",
-    "Cnt",
-    "Cmp",
-    "Cro",
-    "Dec",
-    "Det",
-    "Dri",
-    "Fin",
-    "Fir",
-    "Fla",
-    "Han",
-    "Hea",
-    "Jum",
-    "Kic",
-    "Ldr",
-    "Lon",
-    "Mar",
-    "OtB",
-    "Pac",
-    "Pas",
-    "Pos",
-    "Ref",
-    "Spd",
-    "Work",
-    "Sta",
-    "Str",
-    "Tck",
-    "Tea",
-    "Tec",
-    "Thr",
-    "TRO",
-    "Vis",
-    "Wor",
-    "UID",
-    "Cor",
-    "Club",
-]
+output_dir_label = tk.Label(root, text="Output Directory: Not Selected")
+output_dir_label.pack(pady=10)
 
 
-# Add the views from the "views.json" to the final views list if its in the allowed list
-try:
-    with open("views.json", "r") as views_json:
-        core_views = json.load(views_json)
-except FileNotFoundError:
-    print ("Could not find the views.json file")
-for view_item in core_views["order"]:
-    if view_item in allowed_view_items:
-        final_view_headers.append(view_item)
-    else:
-        print(f"Ignoring {view_item} since it is not in the allowed views list")
+action_button = tk.Button(root, text="Generate", command=generate_html)
+action_button.pack(pady=10)
 
-# Load position weights from JSON file
-try:
-    with open("positions.json", "r") as json_file:
-        position_weights = json.load(json_file)
-
-        # Retrieve position category from JSON configuration
-        position_category = list(
-            position_weights["positions"].keys()
-        )  # Get the key from the "positions" dictionary
-        for position in position_category:
-            squad_rawdata[f"{position}"] = 0
-            # Now get each scoring category from the json ie: "core, essential etc"
-            scoring_categories = list(position_weights["positions"][position].keys())
-            for category in scoring_categories:
-                # Add the data back to the data frame after we have run some calculations on it
-                squad_rawdata[f"{position}"] += weight_calc(position, category)
-            squad_rawdata[position] = squad_rawdata[position].round(1)
-            final_view_headers.append(position)
-except FileNotFoundError:
-    print ("Could not find the positions.json file")
-
-
-squad = squad_rawdata[final_view_headers]
-# Create the dir if it does not exist
-if not os.path.exists('generated_data'):
-   os.makedirs('generated_data')
-# generates random file name for write-out of html file based on the file we use
-
-filename = str(f"generated_data/{latest_filename}-{uuid.uuid4()}.html")
-# creates a sortable html export from the dataframe 'squad'
-html = generate_html(squad)
-open(filename, "w", encoding="utf-8").write(html)
+# Start the Tkinter main loop
+root.mainloop()
